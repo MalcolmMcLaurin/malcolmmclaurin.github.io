d235882e8c3cade04e054add2b61ae4ac4639069
MalcolmMcLaurin <malcolm.jethimclaurin@gmail.com>
2022-11-21 18:19:19 -0500

    Updated mobile site
    Comes with problems: back button only works once

--- mobile.js
+++ mobile.js
@@ -1,8 +1,9 @@
-var svp = document.getElementById("sidepanelserve");
-var sp = document.getElementById("sidepanel");
-
+var svp = document.getElementById("sidepanelserve3");
+var sp = document.getElementById("sidepanel3");
 
 var cpl = document.getElementById("cpl");
+
+var prof = document.getElementById("prof");
 
 var sv = document.getElementById("serve");
 var set = document.getElementById("set");
@@ -23,9 +24,13 @@ var wsde = document.getElementById("wsde");
 var wstr = document.getElementById("wstr");
 var wsqu = document.getElementById("wsqu");
 
+var blk = document.getElementById("block");
 var lin = document.getElementById("linx");
 
 var bkgif = document.getElementById("bkgif");
+
+var lb = document.getELementById("linxbox");
+var al = document.getELementById("al");
 
 var ready = false;
 
@@ -39,7 +44,48 @@ function displayPanel2() {
   fp.style.opacity = "0";
 }
 
+function displayPanel3() {
+  sp.style.color = "black";
+  prof.style.color = "white";
+  svp.style.opacity = "1";
+  sp.style.opacity = "1";
+  svp.style.zIndex = "999";
+  sp.style.zIndex = "999";
+  sv.style.opacity = "0";
+  set.style.opacity = "0"; 
+  fp.style.opacity = "0";
+
+  ws.style.opacity = "0";
+  wsde.style.opacity = "0";
+  wstr.style.opacity = "0";
+  wsqu.style.opacity = "0";
+
+  blk.style.background = "black";
+  blk.style.border = "3px solid black";
+
+  svp.style.left = "0px";
+  svp.style.width = "100%";
+  svp.style.margin = "0 auto";
+
+  sp.style.left = "50%";
+  sp.style.transform = "translate(-50%, -50%)";
+  sp.style.width = "100%";
+  sp.style.top = "50%";
+}
+
+function wait(){
+  displayPanel3();
+  setTimeout(() => { displayPanel3(); }, 500);
+}
+
+function back(){
+  wait();
+}
+
 function start() {
+  blk.style.zIndex = "20";
+  blk.style.background = "transparent";
+  blk.style.border = "none";
   svp.style.left = "100%";
   svp.style.width = "40%";
   svp.style.backgroundImage =
@@ -52,10 +98,13 @@ function start() {
   sp.style.width = "40%";
   sp.style.color = "white";
 
+  sp.style.opacity = "0";
+  svp.style.opacity = "0";
+
   sv.style.opacity = "0";
   fp.style.opacity = "0";
 
-  ws.style.width = "100%";
+  ws.style.width = "80%";
   ws.style.opacity = "1";
   ws.style.left = "0px";
 
@@ -63,7 +112,7 @@ function start() {
   wstr.style.opacity = "0";
   wsqu.style.opacity = "0";
 
-  ws.style.zIndex = "0";
+  ws.style.zIndex = "1";
   wsde.style.zIndex = "-1";
   wstr.style.zIndex = "-1";
   wsqu.style.zIndex = "-1";
@@ -72,16 +121,19 @@ function start() {
   wstr.style.left = "0px";
   wsqu.style.left = "0px";
 
-  wsde.style.width = "100%";
-  wstr.style.width = "100%";
-  wsqu.style.width = "100%";
+  wsde.style.width = "80%";
+  wstr.style.width = "80%";
+  wsqu.style.width = "80%";
 
   bkgif.style.opacity = "0";
   lin.style.zIndex = "999";
-  lin.style.left = "0px";
+  lin.style.right = "0px";
 }
 
 function start2() {
+  blk.style.zIndex = "20";
+  blk.style.background = "transparent";
+  blk.style.border = "none";
   svp.style.left = "100%";
   svp.style.width = "40%";
   svp.style.backgroundImage =
@@ -94,10 +146,13 @@ function start2() {
   sp.style.width = "40%";
   sp.style.color = "white";
 
+  sp.style.opacity = "0";
+  svp.style.opacity = "0";
+
   sv.style.opacity = "0";
   fp.style.opacity = "0";
 
-  wsde.style.width = "100%";
+  wsde.style.width = "80%";
   wsde.style.opacity = "1";
   wsde.style.left = "0px";
 
@@ -106,24 +161,27 @@ function start2() {
   wsqu.style.opacity = "0";
 
   ws.style.zIndex = "-1";
-  wsde.style.zIndex = "0";
+  wsde.style.zIndex = "1";
   wstr.style.zIndex = "-1";
   wsqu.style.zIndex = "-1";
 
-  ws.style.left = "0px";
+  ws.style.right = "0px";
   wstr.style.left = "0px";
   wsqu.style.left = "0px";
 
-  ws.style.width = "100%";
-  wstr.style.width = "100%";
-  wsqu.style.width = "100%";
+  ws.style.width = "80%";
+  wstr.style.width = "80%";
+  wsqu.style.width = "80%";
 
   bkgif.style.opacity = "0";
   lin.style.zIndex = "999";
-  lin.style.left = "0px";
+  lin.style.right = "0px";
 }
 
 function start3() {
+  blk.style.zIndex = "20";
+  blk.style.background = "transparent";
+  blk.style.border = "none";
   svp.style.left = "100%";
   svp.style.width = "40%";
   svp.style.backgroundImage =
@@ -136,10 +194,13 @@ function start3() {
   sp.style.width = "40%";
   sp.style.color = "white";
 
+  sp.style.opacity = "0";
+  svp.style.opacity = "0";
+
   sv.style.opacity = "0";
   fp.style.opacity = "0";
 
-  wstr.style.width = "100%";
+  wstr.style.width = "80%";
   wstr.style.opacity = "1";
   wstr.style.left = "0px";
 
@@ -149,23 +210,26 @@ function start3() {
 
   ws.style.zIndex = "-1";
   wsde.style.zIndex = "-1";
-  wstr.style.zIndex = "0";
+  wstr.style.zIndex = "1";
   wsqu.style.zIndex = "-1";
 
   wsde.style.left = "0px";
   ws.style.left = "0px";
   wsqu.style.left = "0px";
 
-  wsde.style.width = "100%";
-  ws.style.width = "100%";
-  wsqu.style.width = "100%";
+  wsde.style.width = "80%";
+  ws.style.width = "80%";
+  wsqu.style.width = "80%";
 
   bkgif.style.opacity = "0";
   lin.style.zIndex = "999";
-  lin.style.left = "0px";
+  lin.style.right = "0px";
 }
 
 function start4() {
+  blk.style.zIndex = "20";
+  blk.style.background = "transparent";
+  blk.style.border = "none";
   svp.style.left = "100%";
   svp.style.width = "40%";
   svp.style.backgroundImage =
@@ -178,10 +242,13 @@ function start4() {
   sp.style.width = "40%";
   sp.style.color = "white";
 
+  sp.style.opacity = "0";
+  svp.style.opacity = "0";
+
   sv.style.opacity = "0";
   fp.style.opacity = "0";
 
-  wsqu.style.width = "100%";
+  wsqu.style.width = "80%";
   wsqu.style.opacity = "1";
   wsqu.style.left = "0px";
 
@@ -192,17 +259,18 @@ function start4() {
   ws.style.zIndex = "-1";
   wsde.style.zIndex = "-1";
   wstr.style.zIndex = "-1";
-  wsqu.style.zIndex = "0";
+  wsqu.style.zIndex = "1";
 
   wsde.style.left = "0px";
   wstr.style.left = "0px";
   ws.style.left = "0px";
 
-  wsde.style.width = "100%";
-  wstr.style.width = "100%";
-  ws.style.width = "100%";
+  wsde.style.width = "80%";
+  wstr.style.width = "80%";
+  ws.style.width = "80%";
 
   bkgif.style.opacity = "0";
   lin.style.zIndex = "999";
-  lin.style.left = "0px";
+  lin.style.right = "0px";
 }
+
